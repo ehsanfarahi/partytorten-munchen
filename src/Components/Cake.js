d2a82acf944879cf258aab54ef123571c2ad6bcc
Ehsan Farahi <ehsanullah.farahi12@gmail.com>
2023-09-13 21:38:43 +0200

    cake page change again

--- src/Components/Cake.js
+++ src/Components/Cake.js
@@ -250,50 +250,7 @@ export default function Cake() {
             <div>
               {displayContent ? (
                 <>
-                  <div className="birthday-cakes">
-                    <h2>Chocolate Cakes</h2>
-                    <div id="slider-container" className="slider">
-                      {cakes
-                        .filter((fCake) => fCake.category === "Chocolate Cakes")
-                        .map((cake, i) => {
-                          return (
-                            <div
-                              key={cake._id}
-                              className="slide"
-                              onClick={handlSlide}
-                            >
-                              <img
-                                src={require(`../../../server/uploads/cakesImages/${cake.image}`)}
-                                alt={cake.type}
-                              />
-                              <div className="img-overlay">
-                                <p>{cake.type}</p>
-                                <p>Weight: {cake.weight} kg</p>
-                                <p>Price: $ {cake.price}</p>
-                              </div>
-                            </div>
-                          );
-                        })}
-                      <div onClick={prev} className="control-prev-btn">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          height="1em"
-                          viewBox="0 0 320 512"
-                        >
-                          <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-                        </svg>
-                      </div>
-                      <div onClick={next} className="control-next-btn">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          height="1em"
-                          viewBox="0 0 320 512"
-                        >
-                          <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-                        </svg>
-                      </div>
-                    </div>
-                  </div>{" "}
+                  <h2>Cakes</h2>
                 </>
               ) : (
                 <>
